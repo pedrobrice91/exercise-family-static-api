fbf319be7489e64e06860e3f1b6635931eeb6a41
Pedro L Briceno <66653154+pedrobrice91@users.noreply.github.com>
2024-08-17 21:37:16 +0000

    hola

--- src/app.py
+++ src/app.py
@@ -27,16 +27,27 @@ def sitemap():
 
 @app.route('/members', methods=['GET'])
 def handle_hello():
-
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+    print(members)
+    return jsonify(members)
+   
+    
+@app.route('/member', methods=['POST'])
+def method_post():   
+    request_body = request.json
+    jackson_family.add_member(request_body)
+    return jsonify(request_body)
+    
+@app.route('/member/<int:id>', methods=['GET', 'DELETE'])
+def get_member_by_id(id):
+    if request.method == 'GET':
+        member = jackson_family.get_member(id)
+        return jsonify(member)
+    elif request.method == 'DELETE': 
+        members = jackson_family.delete_member(id)
+        return jsonify(members)
 
-
-    return jsonify(response_body), 200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
